faf7ed8b37ceb47379b9dbdf67d1d93470b08982
Valera3107 <valerapetruna3107@gmail.com>
2018-05-16 17:33:38 +0300

    final changes of js file

--- js/module-05/index.js
+++ js/module-05/index.js
@@ -5,20 +5,29 @@ function SocialBook (users, posts) {
   this.posts = posts;
 
   this.getAllUsers = () => {return this.users.map(user => user)};
-  this.getUserByLogin = (login) => { return this.users.find(login => login === login)};
-  this.getUserStatus = (userId) => { return this.users.find(id => id.id === userId).isActive?'active':'inactive'};
+  this.getUserByLogin = (login) => { return this.users.find(user => user.login === login)};
+  this.getUserStatus = (userId) => { return this.users.find(user => user.id === userId).isActive?'active':'inactive'};
   this.addUser = (user) => {return this.users.push(user)};
-  this.removeUserById = (userId) => {return delete this.users[this.users.find(id => id.id === userId)]};
+  this.removeUserById = (userId) => {return this.users.filter(user => user.id !== userId)};
   this.getUsersCount = () => {return this.users.reduce((acc) => acc + 1, 0) };
- }
+
+
+  this.getUserPosts = (userId) => {return this.posts[userId]};
+  this.addPost = (userId, post) => {return this.posts[userId].push(post)};
+  this.removePost = (userId, postId) => {return this.posts[userId].filter(post => post.id !== postId)};
+  this.getAllLikes = (userId) => {return this.posts[userId].reduce((acc, value) => acc + value.likes , 0)};
+  this.addPostLike = (userId, postId) => {return this.posts[userId].find(post => post.id === postId).likes + 1};
+  this.getPostsCount = (userId) => {return this.posts[userId].reduce((acc) => acc + 1, 0)};
+};
 
  const getId = () => "-" + Math.random().toString(36).substr(2, 9);
 
  const initialUsers = [
   { id: "-s19a6hqce", login: "mangozedog@mail.com", password: "qwe123zv", isActive: true },
   { id: "-qkpzenjxe", login: "polysweet@skynet.ze", password: "123zxc78", isActive: true },
-  { id: "-e51cpd4di", login: "ajax2k@change.ua", password: "ert234qw", isActive: false }
+  { id: "-e51cpd4di", login: "ajax2k@change.ua", password: "ert234qw", isActive: false },
 ];
+
 
 const initialPosts = {
   "-s19a6hqce": [
@@ -46,6 +55,12 @@ const newUser = {
   isActive: false
 };
 
+const newPost = {
+  id: getId(),
+   text: "post #($)", 
+   likes: 234
+};
+
 console.log(manager.getAllUsers());
 
 console.log(manager.getUserByLogin('polysweet@skynet.ze'));
@@ -56,7 +71,19 @@ console.log(manager.addUser(newUser));
 console.log(initialUsers);
 
 console.log(manager.removeUserById('-qkpzenjxe'));
-console.log(initialUsers);
 
 console.log(manager.getUsersCount());
 
+//==========================
+
+console.log(manager.getUserPosts('-qkpzenjxe'));
+
+console.log(manager.addPost('-qkpzenjxe', newPost));
+
+console.log(manager.removePost('-s19a6hqce','-hy0eyw5qo'));
+
+console.log(manager.getAllLikes('-e51cpd4di'));
+
+console.log(manager.addPostLike('-e51cpd4di','-i03pbhy3s'));
+
+console.log(manager.getPostsCount('-s19a6hqce'));
