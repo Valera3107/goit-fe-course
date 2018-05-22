5d7c878d47dcd9c96c0ee739145c732d893d0097
Valera3107 <valerapetruna3107@gmail.com>
2018-05-22 21:24:10 +0300

    adds 4 task to additional module

--- js/module-06/additional-module/index.js
+++ js/module-06/additional-module/index.js
@@ -1,55 +1,78 @@
 'use strict';
 
 //=============(1)
-function Account(login, email, friendsCount) {
-  this.login = login;
-  this.email = email;
-  this.friendsCount = friendsCount;
-};
+// function Account(login, email, friendsCount) {
+//   this.login = login;
+//   this.email = email;
+//   this.friendsCount = friendsCount;
+// };
 
-Account.prototype.getAccountInfo = function() {
-  console.log(`Your account has ${this.login} login, ${this.email} email and ${this.friendsCount} friendsCount.`);
-};
+// Account.prototype.getAccountInfo = function() {
+//   console.log(`Your account has ${this.login} login, ${this.email} email and ${this.friendsCount} friendsCount.`);
+// };
 
-const mango = new Account('mango', 'mango@ss.com', 234);
+// const mango = new Account('mango', 'mango@ss.com', 234);
 
-const maximus = new Account('maximus', 'maximus@ai.com', 364);
-mango.getAccountInfo();
-maximus.getAccountInfo();
+// const maximus = new Account('maximus', 'maximus@ai.com', 364);
+// mango.getAccountInfo();
+// maximus.getAccountInfo();
 
-//===============(2)
- function Array(arr) {
-   this.arr = arr;
- };
+// //===============(2)
+//  function Array(arr) {
+//    this.arr = arr;
+//  };
 
- Array.prototype.getLastElem = function() {
-   console.log(this.arr.pop());
- };
+//  Array.prototype.getLastElem = function() {
+//    console.log(this.arr.pop());
+//  };
 
-  const randomNumbers = new Array([1, 4, 5, 45, 3]);
+//   const randomNumbers = new Array([1, 4, 5, 45, 3]);
 
- randomNumbers.getLastElem();
+//  randomNumbers.getLastElem();
 
- //=============(3)
- function Clock(time) {
-   this.time = time;
- }
+//  //=============(3)
+//  function Clock(time) {
+//    this.time = time;
+//  }
   
- Clock.prototype.showTime = function() {console.log(`showTime - ${this.time}`)};
+//  Clock.prototype.showTime = function() {console.log(`showTime - ${this.time}`)};
 
 
 
- function Timer(time, interval) {
-   Clock.call(this, time);
-   this.interval = interval;
- };
- Timer.prototype = Object.create(Clock.prototype);
- Timer.prototype.constructor = Timer;
+//  function Timer(time, interval) {
+//    Clock.call(this, time);
+//    this.interval = interval;
+//  };
+//  Timer.prototype = Object.create(Clock.prototype);
+//  Timer.prototype.constructor = Timer;
 
- Timer.prototype.countTimer = function() {console.log(`countTimer - ${this.interval}`)};
+//  Timer.prototype.countTimer = function() {console.log(`countTimer - ${this.interval}`)};
 
 
- const getTimeValue = new Timer(2, 10);
+//  const getTimeValue = new Timer(2, 10);
 
-getTimeValue.showTime();
-getTimeValue.countTimer();
+// getTimeValue.showTime();
+// getTimeValue.countTimer();
+
+//================(4)
+
+class Account {
+
+  constructor (login, email, friendsCount){
+    this.login = login;
+    this.email = email;
+    this.friendsCount = friendsCount;
+  };
+
+  getAccountInfo(){
+    console.log(`Your account login: ${this.login}, 
+    email: ${this.email}, 
+    friendsCount: ${this.friendsCount}`);
+  }
+};
+
+const ajax = new Account('ajax', 'ajax@x.com', 47);
+ajax.getAccountInfo();
+
+const valley = new Account('valley', 'valley@vv.ua', 2354);
+valley.getAccountInfo();
