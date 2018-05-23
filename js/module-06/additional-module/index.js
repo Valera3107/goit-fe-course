2ba783f6fb09124a1e278bb14123a291a8ce5c8e
Valera3107 <valerapetruna3107@gmail.com>
2018-05-23 18:34:07 +0300

    adds 5 task into additional module

--- js/module-06/additional-module/index.js
+++ js/module-06/additional-module/index.js
@@ -56,23 +56,59 @@
 
 //================(4)
 
-class Account {
+// class Account {
 
-  constructor (login, email, friendsCount){
-    this.login = login;
-    this.email = email;
-    this.friendsCount = friendsCount;
-  };
+//   constructor (login, email, friendsCount){
+//     this.login = login;
+//     this.email = email;
+//     this.friendsCount = friendsCount;
+//   };
 
-  getAccountInfo(){
-    console.log(`Your account login: ${this.login}, 
-    email: ${this.email}, 
-    friendsCount: ${this.friendsCount}`);
+//   getAccountInfo(){
+//     console.log(`Your account login: ${this.login}, 
+//     email: ${this.email}, 
+//     friendsCount: ${this.friendsCount}`);
+//   }
+// };
+
+// const ajax = new Account('ajax', 'ajax@x.com', 47);
+// ajax.getAccountInfo();
+
+// const valley = new Account('valley', 'valley@vv.ua', 2354);
+// valley.getAccountInfo();
+
+//===================(5)
+
+class Clock {
+  constructor(time){
+    this.time = time;
+  }
+
+  showTime(){
+    console.log(`Time - ${this.time}`);
+  }
+
+  static convertTime(ms){
+    return new Date(ms);
   }
 };
 
-const ajax = new Account('ajax', 'ajax@x.com', 47);
-ajax.getAccountInfo();
+class Timer extends Clock{
+  constructor(time, interval){
+    super(time);
+    this.interval = interval;
+  };
 
-const valley = new Account('valley', 'valley@vv.ua', 2354);
-valley.getAccountInfo();
+  countTime(){
+    console.log(`Interval - ${this.interval}`);
+  }
+}
+
+const valueOfTimer = new Timer(232121, 32);
+valueOfTimer.showTime();
+valueOfTimer.countTime();
+
+const valueOfClock = new Clock(123);
+valueOfClock.showTime();
+
+console.log(Clock.convertTime(45453));
