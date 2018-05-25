5a18b72d60fb980753b069a0349c55ab780320bd
Valera3107 <valerapetruna3107@gmail.com>
2018-05-25 20:04:53 +0300

    makes objects of Humburger

--- js/module-06/module/index.js
+++ js/module-06/module/index.js
@@ -215,63 +215,63 @@ class Hamburger {
   constructor(size, stuffing){
       this.size = size;
       this.stuffing = stuffing;
-  }
 
-   SIZE_SMALL = 'SIZE_SMALL';
-   SIZE_LARGE = 'SIZE_LARGE';
-
-   SIZES = {
-    [Hamburger.SIZE_SMALL]: {
-      price: 30,
-      calories: 50,
-    },
-    [Hamburger.SIZE_LARGE]: {
-      price: 50,
-      calories: 100,
-    },
+      Hamburger.SIZE_SMALL = 'SIZE_SMALL';
+      Hamburger.SIZE_LARGE = 'SIZE_LARGE';
+   
+      Hamburger.SIZES = {
+       [Hamburger.SIZE_SMALL]: {
+         price: 30,
+         calories: 50,
+       },
+       [Hamburger.SIZE_LARGE]: {
+         price: 50,
+         calories: 100,
+       },
+     };
+     
+     Hamburger.STUFFING_CHEESE = 'STUFFING_CHEESE';
+     Hamburger.STUFFING_SALAD = 'STUFFING_SALAD';
+     Hamburger.STUFFING_MEAT = 'STUFFING_MEAT';
+     
+     Hamburger.STUFFINGS = {
+     [Hamburger.STUFFING_CHEESE]: {
+       price: 15,
+       calories: 20,
+     },
+     [Hamburger.STUFFING_SALAD]: {
+       price: 20,
+       calories: 5,
+     },
+     [Hamburger.STUFFING_MEAT]: {
+       price: 35,
+       calories: 15,
+     },
+   };
+   
+   Hamburger.TOPPING_SPICE = 'TOPPING_SPICE';
+   Hamburger.TOPPING_SAUCE = 'TOPPING_SAUCE';
+   
+   Hamburger.TOPPINGS = {
+     [Hamburger.TOPPING_SPICE]: {
+       price: 10,
+       calories: 0,
+     },
+     [Hamburger.TOPPING_SAUCE]: {
+       price: 5,
+       calories: 100,
+     }
+   };
   };
-  
-  STUFFING_CHEESE = 'STUFFING_CHEESE';
-  STUFFING_SALAD = 'STUFFING_SALAD';
-  STUFFING_MEAT = 'STUFFING_MEAT';
-  
-  STUFFINGS = {
-  [Hamburger.STUFFING_CHEESE]: {
-    price: 15,
-    calories: 20,
-  },
-  [Hamburger.STUFFING_SALAD]: {
-    price: 20,
-    calories: 5,
-  },
-  [Hamburger.STUFFING_MEAT]: {
-    price: 35,
-    calories: 15,
-  },
-};
 
-TOPPING_SPICE = 'TOPPING_SPICE';
-TOPPING_SAUCE = 'TOPPING_SAUCE';
 
-TOPPINGS = {
-  [Hamburger.TOPPING_SPICE]: {
-    price: 10,
-    calories: 0,
-  },
-  [Hamburger.TOPPING_SAUCE]: {
-    price: 5,
-    calories: 100,
-  }
-};
 
 };
-
 
 // добавляем много добавок
 const h4 = new Hamburger({
-  size: Hamburger.SIZE_SMALL, 
+  size: [Hamburger.SIZE_SMALL], 
   stuffing: Hamburger.STUFFING_CHEESE
 });
 				   
-hamburger.addTopping(Hamburger.TOPPING_SAUCE);
-hamburger.addTopping(Hamburger.TOPPING_SAUCE); 
+console.log(h4);
