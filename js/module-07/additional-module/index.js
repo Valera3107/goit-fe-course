8d019fbdd7258aace4a05ba60909d2ed55530935
Valera3107 <valerapetruna3107@gmail.com>
2018-06-05 17:27:57 +0300

    adds 1-3 tasks at additional module

--- js/module-07/additional-module/index.js
+++ js/module-07/additional-module/index.js
@@ -1,3 +1,39 @@
 'use strict';
 
 //===========(1)
+// const categoryList = document.querySelectorAll("ul");
+
+// console.log(categoryList);
+//=========(2)
+// const getCheckedInputsData = function(inputs){
+//   const arrOfCheckElem = Array.from(inputs);
+//   const checkedElem = arrOfCheckElem.filter(val => val.hasAttribute('checked'));
+//   let objOfValues = {
+//       values: [],
+//       prop: []
+//   };
+//   checkedElem.map(val => objOfValues.values.push(val.getAttribute('value')));
+//   checkedElem.find(val => objOfValues.prop.push(val.getAttribute('data-prop')));
+//   console.log(objOfValues);
+// }; 
+
+// let categoryListInputs = Array.from(document.querySelectorAll('input'));
+// getCheckedInputsData(categoryListInputs);
+
+//==========(3)
+function makerList(){
+  let textContent;
+  do{
+  textContent = prompt('Input text for item:');
+  let newItem = document.createElement('li');
+  newItem.textContent = textContent;
+  elementUl.appendChild(newItem);
+  }while(textContent);
+
+};
+
+const list = document.querySelector('#list');
+const elementUl = document.createElement('ul');
+
+makerList();
+list.appendChild(elementUl);
