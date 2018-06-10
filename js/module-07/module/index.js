47e2d69deff296431caabea9d20d552372774b4c
Valera3107 <valerapetruna3107@gmail.com>
2018-06-10 18:49:06 +0300

    adds final changes

--- js/module-07/module/index.js
+++ js/module-07/module/index.js
@@ -72,57 +72,99 @@ function createPostAction(iconClass, count){
   return createItem;
 };
 
+//=====(2)
+// function createPostCard({img = 'http://...', title = 'Some text ...', text = 'Lorem ...', stats = {likes: 0, dislike: 0, fav: 0}}){
+// const card = document.createElement('div');
+// card.classList.add('post');
 
-function createPostCard({img = 'http://...', title = 'Some text ...', text = 'Lorem ...', stats = {likes: 0, dislike: 0, fav: 0}}){
-const card = document.createElement('div');
-card.classList.add('post');
+// //====img
+// const createImg = document.createElement('img');
 
-//====img
-const createImg = document.createElement('img');
+// createImg.setAttribute('class', 'post__image');
+// createImg.setAttribute('src', img);
+// createImg.setAttribute('alt', 'post image');
+// card.appendChild(createImg);
 
-createImg.setAttribute('class', 'post__image');
-createImg.setAttribute('src', img);
-createImg.setAttribute('alt', 'post image');
-card.appendChild(createImg);
+// //====title
+// const createTitle = document.createElement('h2');
+// createTitle.classList.add('post__title');
+// createTitle.textContent = title;
+// card.appendChild(createTitle);
 
-//====title
-const createTitle = document.createElement('h2');
-createTitle.classList.add('post__title');
-createTitle.textContent = title;
-card.appendChild(createTitle);
+// //====text
+// const createText = document.createElement('p');
+// createText.classList.add('post__text');
+// createText.textContent = text;
+// card.appendChild(createText);
 
-//====text
-const createText = document.createElement('p');
-createText.classList.add('post__text');
-createText.textContent = text;
-card.appendChild(createText);
+// //=====List
+// const createList = document.createElement('ul');
+// createList.classList.add('actions');
+// createList.classList.add('post__actions');
 
-//=====List
-const createList = document.createElement('ul');
-createList.classList.add('actions');
-createList.classList.add('post__actions');
-
-//==li
-const postActionsData = [
-  {iconClass: 'actions__icon--like', count: stats.likes},
-  {iconClass: 'actions__icon--dislike', count: stats.dislikes},
-  {iconClass: 'actions__icon--fav', count: stats.fav}
-];
-
-
-postActionsData.forEach(action => {
-  const elem = createPostAction(action.iconClass, action.count);
-  createList.appendChild(elem);
-})
+// //==li
+// const postActionsData = [
+//   {iconClass: 'actions__icon--like', count: stats.likes},
+//   {iconClass: 'actions__icon--dislike', count: stats.dislikes},
+//   {iconClass: 'actions__icon--fav', count: stats.fav}
+// ];
 
 
-card.appendChild(createList);
-return card;
-};
+// postActionsData.forEach(action => {
+//   const elem = createPostAction(action.iconClass, action.count);
+//   createList.appendChild(elem);
+// })
 
 
-function createCards(posts){
-  posts.forEach(post => sectionCard.appendChild(createPostCard(post)));;
+// card.appendChild(createList);
+// return card;
+// };
+
+// //==========(3)
+// function createCards(posts){
+//   posts.forEach(post => sectionCard.appendChild(createPostCard(post)));;
+// }
+
+
+//====(1)
+const card = createCards(posts);
+sectionCard.innerHTML = card;
+
+
+function createCards(arr) {
+  let elements = '';
+
+  return arr.reduce((acc, obj) => acc + createPostCard(obj), '');
 }
 
+
 createCards(posts);
+
+
+function createPostCard({ img, title, text, stats }) {
+  return `<div class="post">
+  <img class="post__image" src=${img} alt="post image">
+  <h2 class="post__title">${title}</h2>
+  <p class="post__text">${text}</p>
+  <ul class="actions post__actions">
+    <li class="actions__item">
+      <button class="actions__btn ">
+        <span class="actions__icon actions__icon--like"></span>
+        <span class="actions__count">${stats.likes}</span>
+      </button>
+    </li>
+    <li class="actions__item">
+      <button class="actions__btn">
+        <span class="actions__icon actions__icon--dislike"></span>
+        <span class="actions__count">${stats.dislikes}</span>
+      </button>
+    </li>
+    <li class="actions__item">
+      <button class="actions__btn">
+         <span class="actions__icon actions__icon--fav"></span>
+        <span class="actions__count">${stats.fav}</span>
+      </button>
+    </li>
+  </ul>
+</div>`;
+}
