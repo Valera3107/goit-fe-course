03d790812b1d59eb4ded6c33a96840d80da5fa17
Valera3107 <valerapetruna3107@gmail.com>
2018-06-22 10:00:19 +0300

    adds task 4 to additional module

--- js/module-09/additional-module/index.js
+++ js/module-09/additional-module/index.js
@@ -2,7 +2,35 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    // ======(1)
+  // ==========(1)
+//   const startBtn = document.querySelector('.js-start');
+//   const stopBtn = document.querySelector('.js-stop');
+//   const background = document.querySelector('body');
+
+//   startBtn.addEventListener('click', changeBackground);
+//   stopBtn.addEventListener('click', stopChangeBackground);
+
+//   let timerId;
+
+//   function changeBackground() {
+//     timerId = setInterval(()=>{
+//       background.style.backgroundColor = getRandomItem(colors);
+//     }, 1000);
+//   }
+
+//   function stopChangeBackground() {
+//      clearInterval(timerId);
+//   }
+
+
+//   const colors = ['#FFFFFF', '#F44336', '#2196F3', '#4CAF50', '#FF9800', '#009688', '#795548'];
+
+//   function getRandomItem(list){
+//     return list[Math.floor(Math.random() * (list.length))];
+// }
+
+
+    // ======(2)
     // function getFormattedTime(time) {
     //     let date = new Date(time);
     //     const min = date.getMinutes();
@@ -25,72 +53,111 @@ document.addEventListener('DOMContentLoaded', () => {
     //   ); // 07:24.2
 
 
-    //    ===================(2)
-    const clockface = document.querySelector(".js-clockface");
-    const startBtn = document.querySelector(".js-timer-start");
-    const stopBtn = document.querySelector(".js-timer-stop");
+    //    ===================(3)
+    // const clockface = document.querySelector(".js-clockface");
+    // const startBtn = document.querySelector(".js-timer-start");
+    // const stopBtn = document.querySelector(".js-timer-stop");
 
 
-    const timer = {
-        startTime: null,
-        deltaTime: null,
-        id: null,
-        isActive: false,
-        startTimer() {
-          if(!this.isActive){
-          this.startTime = Date.now();
-          this.isActive = true;
-          this.id = setInterval(()=>{
-          const currentTime = Date.now();
-          this.deltaTime = currentTime - this.startTime;
-          const time = new Date(this.deltaTime);
-          updateClockface(clockface, time);
-          }, 100);
-          }
-        },
-        stopTimer() {
-          this.isActive = false;
-          clearInterval(this.id);  
-          this.id = null;
-          this.startTime = null;
-          this.deltaTime = null; 
-          const time = new Date(this.deltaTime);
-          updateClockface(clockface, time);
-        }
-    };
+    // const timer = {
+    //     startTime: null,
+    //     deltaTime: null,
+    //     id: null,
+    //     isActive: false,
+    //     startTimer() {
+    //       if(!this.isActive){
+    //       this.startTime = Date.now();
+    //       this.isActive = true;
+    //       this.id = setInterval(()=>{
+    //       const currentTime = Date.now();
+    //       this.deltaTime = currentTime - this.startTime;
+    //       const time = new Date(this.deltaTime);
+    //       updateClockface(clockface, time);
+    //       }, 100);
+    //       }
+    //     },
+    //     stopTimer() {
+    //       this.isActive = false;
+    //       clearInterval(this.id);  
+    //       this.id = null;
+    //       this.startTime = null;
+    //       this.deltaTime = null; 
+    //       const time = new Date(this.deltaTime);
+    //       updateClockface(clockface, time);
+    //     }
+    // };
   
-    function getFormattedTime(time) {
-      const min = time.getMinutes();
-      const sec = time.getSeconds();
-      const ms = Number.parseInt((time.getMilliseconds())/100);
-      if(min < 10 && sec < 10){
-        return `0${min}:0${sec}.${ms}`;
-      }
+    // function getFormattedTime(time) {
+    //   const min = time.getMinutes();
+    //   const sec = time.getSeconds();
+    //   const ms = Number.parseInt((time.getMilliseconds())/100);
+    //   if(min < 10 && sec < 10){
+    //     return `0${min}:0${sec}.${ms}`;
+    //   }
 
-      if(min < 10){
-        return `0${min}:${sec}.${ms}`;
-      }
-    }
+    //   if(min < 10){
+    //     return `0${min}:${sec}.${ms}`;
+    //   }
+    // }
    
-    function updateClockface(elem, time) {
-      elem.textContent = getFormattedTime(time);
-    }
+    // function updateClockface(elem, time) {
+    //   elem.textContent = getFormattedTime(time);
+    // }
 
-    function setActiveBtn({target}) {
-      if(target.classList.contains('active')) {
-        return;
-      }
+    // function setActiveBtn({target}) {
+    //   if(target.classList.contains('active')) {
+    //     return;
+    //   }
       
-      startBtn.classList.remove('active');
-      stopBtn.classList.remove('active');
+    //   startBtn.classList.remove('active');
+    //   stopBtn.classList.remove('active');
       
-      target.classList.add('active');
+    //   target.classList.add('active');
+    // }
+
+    // startBtn.addEventListener('click', timer.startTimer.bind(timer));
+    // stopBtn.addEventListener('click', timer.stopTimer.bind(timer));
+
+    // startBtn.addEventListener('click', setActiveBtn);
+    // stopBtn.addEventListener('click', setActiveBtn);
+
+    // ===============(4)
+    const DELAY = 1000;
+
+    let goodsAmount = 100;
+
+    const processOrder = amount => {
+      return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+          if(!Number.isNaN(amount)){
+            if(goodsAmount < amount){
+              resolve(' К сожалению на складе недостаточно товаров!');
+            }
+            goodsAmount -= amount;
+            resolve('Ваш заказ готов!');
+          }
+
+            reject('Некоректный ввод!');
+          
+        }, DELAY / 2)
+      });
     }
-
-    startBtn.addEventListener('click', timer.startTimer.bind(timer));
-    stopBtn.addEventListener('click', timer.stopTimer.bind(timer));
-
-    startBtn.addEventListener('click', setActiveBtn);
-    stopBtn.addEventListener('click', setActiveBtn);
-
+    
+    // Вызовы функции для проверки
+    processOrder(50)
+      .then(x => console.log(x)) // Ваш заказ готов!
+      .catch(err => console.log(err));
+    
+    processOrder(50)
+      .then(x => console.log(x)) // Ваш заказ готов!
+      .catch(err => console.log(err));
+    
+    processOrder(50)
+      .then(x => console.log(x)) // К сожалению на складе недостаточно товаров!
+      .catch(err => console.log(err));
+    
+    processOrder("qwe")
+      .then(x => console.log(x))
+      .catch(err => console.log(err)); // Некоректный ввод!
+    
 });
--- js/module-09/module/index.js
+++ js/module-09/module/index.js
@@ -5,6 +5,75 @@ document.addEventListener('DOMContentLoaded', () => {
     const clockface = document.querySelector(".js-time");
     const startBtn = document.querySelector(".js-start");
     const resetBtn = document.querySelector(".js-reset");
+    const list = document.querySelector('.js-laps');
+
+    // class Stopwatch{
+    //     constructor(){
+    //       this.countClick = 0;
+    //       this.isActive = false;
+    //       this.startTime = null;
+    //       this.id = null;
+    //       this.deltaTime = null,
+    //       this.isStart = false;
+    //       this.time = null;
+    //       this.fixedTime = null;
+    //     }
+
+    //     startTimer(){
+    //         this.countClick += 1;
+    //           if(this.countClick < 4){
+    //             if(!this.isActive && this.countClick === 1){
+    //                this.isActive = true;
+    //                this.isStart = true;
+    //                startBtn.textContent = 'Pause';
+    //                this.startTime = Date.now();
+    //                this.continueTimer();
+    //             }
+    //             else {
+    //               if(this.isStart && this.countClick === 2){
+    //                 this.pauseTimer();
+    //               }
+    //               else {
+    //                 startBtn.textContent = 'Pause';
+    //                 this.countClick = 1;
+    //                  this.startTime = Date.now();
+    //                  this.startTime -= this.fixedTime;
+    //                  this.continueTimer();
+    //               }
+    //             }
+    //           }
+    //       }
+
+    //       pauseTimer() {
+    //         this.fixedTime = this.time;
+    //         startBtn.textContent = 'Continue';
+    //         clearInterval(this.id);
+    //         updateClockface(clockface, this.fixedTime);
+    //       }
+
+    //       continueTimer() {
+    //          this.id = setInterval(()=>{
+    //             const currentTime = Date.now();
+    //             this.deltaTime = currentTime - this.startTime;
+    //             this.time = new Date(this.deltaTime);
+    //             updateClockface(clockface, this.time);
+    //         }, 100);
+    //       }
+
+
+    //       resetTimer(){
+    //         this.countClick = 0;
+    //         clearInterval(this.id);
+    //         startBtn.textContent = 'start';
+    //         this.isActive = false;
+    //         this.id = null;
+    //         this.startTime = null;
+    //         this.deltaTime = null;
+    //         this.time = new Date(this.deltaTime);
+    //         updateClockface(clockface, this.time);
+    //       }
+
+    // };
 
     const timer = {
       countClick: 0,
@@ -23,12 +92,7 @@ document.addEventListener('DOMContentLoaded', () => {
                this.isStart = true;
                startBtn.textContent = 'Pause';
                this.startTime = Date.now();
-               this.id = setInterval(()=>{
-                   const currentTime = Date.now();
-                   this.deltaTime = currentTime - this.startTime;
-                   this.time = new Date(this.deltaTime);
-                   updateClockface(clockface, this.time);
-               }, 100)
+               this.continueTimer();
             }
             else {
               if(this.isStart && this.countClick === 2){
@@ -39,12 +103,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 this.countClick = 1;
                  this.startTime = Date.now();
                  this.startTime -= this.fixedTime;
-                 this.id = setInterval(()=>{
-                    const currentTime = Date.now();
-                    this.deltaTime = currentTime - this.startTime;
-                    this.time = new Date(this.deltaTime);
-                    updateClockface(clockface, this.time);
-                }, 100);
+                 this.continueTimer();
               }
             }
           }
@@ -55,6 +114,16 @@ document.addEventListener('DOMContentLoaded', () => {
         clearInterval(this.id);
         updateClockface(clockface, this.fixedTime);
       },
+
+      continueTimer(){
+        this.id = setInterval(()=>{
+            const currentTime = Date.now();
+            this.deltaTime = currentTime - this.startTime;
+            this.time = new Date(this.deltaTime);
+            updateClockface(clockface, this.time);
+        }, 100);
+      },
+
       resetTimer(){
         this.countClick = 0;
         clearInterval(this.id);
