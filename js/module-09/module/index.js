94f50730f265a20402b591847d1cd983a2f27004
Valera3107 <valerapetruna3107@gmail.com>
2018-06-24 10:55:12 +0300

    delet some useless

--- js/module-09/module/index.js
+++ js/module-09/module/index.js
@@ -17,7 +17,6 @@ document.addEventListener('DOMContentLoaded', () => {
       deltaTime: null,
       time: null,
       fixedTime: null,
-      arrOfLaps: [],
       maxLap: 10,
       countLap: 0,
       
