85ef5f7b656d464b3c06c021093b84639037e0b5
Valera3107 <valerapetruna3107@gmail.com>
2018-06-29 20:21:06 +0300

    adds all functional and styles

--- js/module-10/module/index.js
+++ js/module-10/module/index.js
@@ -1,34 +1,38 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const panels = document.querySelector('.js-tabs__nav');
 
-    /*
-  Написать приложение для работы с REST сервисом, 
-  все функции делают запрос и возвращают Promise 
-  с которым потом можно работать. 
-  
-  Реализовать следующий функционал:
-  - функция getAllUsers() - должна вернуть текущий список всех пользователей в БД.
-  
-  - функция getUserById(id) - должна вернуть пользователя с переданным id.
-  
-  - функция addUser(name, age) - должна записывать в БД юзера с полями name и age.
-  
-  - функция removeUser(id) - должна удалять из БД юзера по указанному id.
-  
-  - функция updateUser(id, user) - должна обновлять данные пользователя по id. 
-    user это объект с новыми полями name и age.
-  Документацию по бэкенду и пример использования прочитайте 
-  в документации https://github.com/trostinsky/users-api#users-api.
-  Сделать минимальный графический интерфейс в виде панели с полями и кнопками. 
-  А так же панелью для вывода результатов операций с бэкендом.
+  const links = document.querySelectorAll('.tabs__link');
 
-  https://test-users-api.herokuapp.com/users/
-*/
+  const tabsPane = document.querySelectorAll('.tabs__pane');
+  
+  panels.addEventListener('click', switchPanel);
 
-// =======================
+  function switchPanel({target}) {
+    event.preventDefault();
+    const nodeName = target.nodeName;
+    const max = links.length;
+    if(nodeName !== 'A') return;
+
+    for(let i = 0; i < max; i++){
+        if(links[i] !== target){
+            links[i].classList.remove('tabs__link--active');
+            tabsPane[i].classList.remove('tabs__pane--active');
+        } else {
+            links[i].classList.add('tabs__link--active');
+            tabsPane[i].classList.add('tabs__pane--active');
+        }
+    }
+  }
+// =================
+const button = document.querySelector('.js-get');
+const table = document.querySelector('table');
+
+button.addEventListener('click', getAllUsers);
+
 function getAllUsers(event){
-  //  event.preventDefault();
+   event.preventDefault();
    fetch('https://test-users-api.herokuapp.com/users/')
    .then(response => {
      if(response.ok) return response.json();
@@ -36,34 +40,48 @@ function getAllUsers(event){
      throw new Error(`Error: ${response.statusText}`);
    })
    .then(data => {
-     console.log(data.data);
+     makeTable(data.data);
    })
    .catch(error => {
      console.log(error);
    })
 }
-
-getAllUsers();
-
-
+ function makeTable(arr) {
+   arr.forEach(elem => {
+   const tr = document.createElement('tr');
+   const itemId = document.createElement('td');
+   const itemName = document.createElement('td');
+   const itemAge = document.createElement('td');
+   itemId.textContent = elem.id;
+   itemId.classList.add('table-item');
+   itemName.textContent = elem.name;
+   itemName.classList.add('table-item');
+   itemAge.textContent = elem.age;
+   itemAge.classList.add('table-item');
+   tr.append(itemId, itemName, itemAge);
+   table.appendChild(tr);
+   })
+ }
 
 // ========================
-getUserById('5b0f01c41744100014006452');
 
-function getUserById(id) {
-  //event.preventDefault();
-  fetch('https://test-users-api.herokuapp.com/users/')
+const input = document.querySelector('.js-search');
+const searchBtn = document.querySelector('#js-submit');
+const userText = document.querySelector('#showUserInfo');
+
+searchBtn.addEventListener('click', getUserById);
+
+
+function getUserById() {
+  event.preventDefault();
+  fetch(`https://test-users-api.herokuapp.com/users/${input.value}`)
   .then(response => {
     if(response.ok) return response.json();
 
     throw new Error(`Error: ${response.statusText}`);
   })
   .then(data => {
-    data.data.forEach(element => {
-      if(element.id === id){
-        console.log(element);
-      }
-    });
+    userText.textContent = `Name: ${data.data.name}, age: ${data.data.age}`;
   })
   .catch(error => {
     console.log(error);
@@ -72,26 +90,30 @@ function getUserById(id) {
 
 
 // ===================
-const user = {
-  name: 'Ivan',
-  age: 34
-}
+const userName = document.querySelector('.js-name');
+const userAge = document.querySelector('.js-age');
+const submitBtn = document.querySelector('#js-submitNewUser');
+const textAboutNewUser = document.querySelector("#newUserInfotmation");
 
-addUser(user);
+submitBtn.addEventListener('click', addUser);
 
-function addUser(user) {
+function addUser(event) {
+  event.preventDefault();
   fetch('https://test-users-api.herokuapp.com/users/', {
     method: 'POST',
-    body: JSON.stringify(user),
+    body: JSON.stringify({name: userName.value , age: userAge.value}),
     headers: {
-      "Content-type": "application/json; charset=UTF-8"
+      Accept: 'application/json',
+    'Content-Type': 'application/json',
     }
   })
   .then(response => {
-    return response.json();
+    if(response.ok) return response.json();
+    
+    throw new Error(`Error: ${response.statusText}`);
   })
   .then(data => {  
-    console.log(data);
+    textAboutNewUser.textContent = `Successfully added new user: name: ${userName.value}, age: ${userAge.value}`;
   })
   .catch(error => {
     console.log("Error: " + error);
@@ -100,44 +122,50 @@ function addUser(user) {
 
 
 // =========================
-removeUser('5b086645e0400a0014a69c3d');
+const inputForRemove = document.querySelector('.js-remove');
+const removeBtn = document.querySelector('#js-remove');
+const textAboutRemoveUser = document.querySelector('#deleted-info');
 
-function removeUser(id) {
-  fetch(`https://test-users-api.herokuapp.com/users/:${id}`, {
+removeBtn.addEventListener('click', removeUser)
+
+function removeUser(event) {
+  event.preventDefault();
+  fetch(`https://test-users-api.herokuapp.com/users/${inputForRemove.value}`, {
     method: 'DELETE',
   })
   .then(() => {
-    console.log('User was successfully deleted!');
+    textAboutRemoveUser.textContent = `Deleted user by id: ${inputForRemove.value}`;
   })
   .catch(error => {
     console.log("Error: " + error);
   })
 }
 
-
-
 // ==================
-const newInfo = {
-  name: "Viktor",
-  age: 23
-}
 
-updateUser('5b0be6b54f70ae00141efe9c', {newInfo});
+const userId = document.querySelector('#js-resubmit-id');
+const newUserName = document.querySelector('#js-resubmit-name');
+const newUserAge = document.querySelector('#js-resubmit-age');
+const resubmitBtn = document.querySelector('#js-resubmit');
+const newUserInfo = document.querySelector('#resubmitUserInfo');
 
-function updateUser(id, user) {
-  fetch(`https://test-users-api.herokuapp.com/users/:${id}`, {
+resubmitBtn.addEventListener('click', updateUser);
+
+function updateUser(event) {
+  event.preventDefault();
+  fetch(`https://test-users-api.herokuapp.com/users/${userId.value}`, {
     method: 'PUT',
-    body: JSON.stringify(user),
+    body: JSON.stringify({name: newUserName.value, age: newUserAge.value}),
     headers: {
-      "Content-type": "application/json; charset=UTF-8"
+      Accept: 'application/json',
+    'Content-Type': 'application/json',
     }
   })
   .then(response => {
-    console.log(response);
     return response.json();
   })
   .then(data => {
-     console.log(data);
+     newUserInfo.textContent = `New name:${data.data.name}, new age: ${data.data.age}`;
   })
   .catch(error => {
     console.log("Error: " + error);
