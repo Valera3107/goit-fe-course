58a8c291775662391dafc66ca289c03f1d002f83
Valera3107 <valerapetruna3107@gmail.com>
2019-03-21 18:45:45 +0200

    adds some fixing

--- additional-js/module-02/index.js
+++ additional-js/module-02/index.js
@@ -1,12 +1,12 @@
 "use strict"
 
 let arr = [];
-
-do{
-  const input = parseInt(prompt("Input number:", "100"));
-  if(input === null) break;
+let input;
+while(true){
+  input = parseInt(prompt("Input number:", "100"));
+  if(input === null ) break;
   else arr.push(input);
-}while(true);
+}
 
 let total = 0;
 
@@ -17,3 +17,38 @@ if(arr.length !== 0){
 }
 
 alert("Total sum = " + total);
+
+
+//=====================
+
+const passwords = ['qwerty', '111qwe', '123123', 'r4nd0mp4zzw0rd'];
+
+let userPassword;
+let checkPassword;
+let userChoice;
+let count = 3;
+
+
+while(count !== 0){
+  userPassword = prompt('Hello. Input your password:');
+  if(userPassword !== null){
+  checkPassword = passwords.includes(userPassword);
+    if(checkPassword){
+      alert('Добро пожаловать!');
+      break;
+    }
+    else{
+      alert('Неверный пароль!');
+      count-=1;
+        if(count > 0){
+          alert(`У вас осталось ${count} попытки!`);
+        }
+        else{
+          alert('У вас закончились попытки, аккаунт заблокирован.');
+        }
+    }
+  }
+  else{
+    break;
+  }
+}
